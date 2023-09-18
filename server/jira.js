8e796719222f1c65f5d9bc51889334bdcb881771
deepakramji <ramji.hariharan@gmail.com>
2023-09-18 16:57:57 +0530

    Removed some credentials

--- server/jira.js
+++ server/jira.js
@@ -46,10 +46,6 @@ updateNotes();
 /*const axios = require("axios");
 
 // Replace with your Jira Cloud domain and API token or password
-const jiraDomain = "https://hermanmiller.atlassian.net";
-const userName = "deepak_ramji@hermanmiller.com";
-const apiKeyOrPassword =
-  "ATATT3xFfGF0iO_PALmE0auSQAHyUQowTPw55lGFADcPzgEdUwvfTKis59hqJRE3ASzCbTguBUyWEDoUQ_PA8c1lzkNxV5m6MLA5QP7Q7sM_yXYZZbfn0lbfRwwW4fKsSG3vlf-IPt0Us7pGaII-nDm7pqYQuz1ajDtFBBcs-3l7YyhdL1YOsjo=765E15F8";
 
 // Define Jira API endpoints
 const baseUrl = `https://${jiraDomain}/rest/api/latest`;
